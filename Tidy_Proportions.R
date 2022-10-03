f7543801a53d44fa6584922ae17927c3e01cc6d1
MarkSTenenbaum <112495365+MarkSTenenbaum@users.noreply.github.com>
2022-10-02 20:43:21 -0400

    Update Tidy_Proportions.R

--- Tidy_Proportions.R
+++ Tidy_Proportions.R
@@ -1,6 +1,15 @@
-propfunc <- function(data, var, weight){
+# weighted proportions
+propfuncwt <- function(data, var, weight){
   probs <- data %>%
     dplyr::count({{var}}, wt = {{weight}}) %>% 
+    mutate(pct = prop.table(n))
+  return(probs)
+}
+
+# unweighted proportions
+propfunc <- function(data, var){
+  probs <- data %>%
+    dplyr::count({{var}}) %>% 
     mutate(pct = prop.table(n))
   return(probs)
 }
